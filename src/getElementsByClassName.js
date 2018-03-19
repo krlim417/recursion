7a576317ce2dd29bab4bfcfb0deb8ab2b36fd93f
krlim417 <krlim417@gmail.com>
2018-03-19 14:22:02 -0700

    Returns elements containing the desired class name

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,16 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  const results = [];
+  const getElementsMatchingClassName = function(element) {
+    if (element.classList && element.classList.contains(className)) {
+      results.push(element);
+    }
+    element.childNodes.forEach(childNode => {
+      getElementsMatchingClassName(childNode);
+    });
+  };
+  getElementsMatchingClassName(document.body, []);
+  return results;
 };
